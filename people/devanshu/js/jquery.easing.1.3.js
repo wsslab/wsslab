fc1acbc4ae2f573b27151f758c2384fd768d8795
Vu Le <leducvuvietnam@gmail.com>
2025-01-15 15:26:13 -0500

    rm binary character

--- people/devanshu/js/jquery.easing.1.3.js
+++ people/devanshu/js/jquery.easing.1.3.js
@@ -8,7 +8,6 @@
  * 
  * Open source under the BSD License. 
  * 
- * Copyright Ã‚Â© 2008 George McGinley Smith
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
@@ -177,7 +176,6 @@ jQuery.extend( jQuery.easing,
  * 
  * Open source under the BSD License. 
  * 
- * Copyright Ã‚Â© 2001 Robert Penner
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without modification, 
